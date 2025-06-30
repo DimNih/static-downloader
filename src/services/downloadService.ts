03bc3c4d2356c45f262315f239bc51d706e27717
DimNih <dd0613987@gmail.com>
2025-06-30 21:24:43 +0700

    first commit

--- src/services/downloadService.ts
+++ src/services/downloadService.ts
@@ -18,7 +18,7 @@ export interface VideoFormat {
 }
 
 class DownloadService {
-  private BACKEND_URL = 'http://localhost:8080';
+  private BACKEND_URL = 'https://bekend-downloader-production.up.railway.app/';
 
   async getVideoInfo(url: string): Promise<VideoInfo> {
     const response = await axios.post<VideoInfo>(`${this.BACKEND_URL}/api/video-info`, { url });
